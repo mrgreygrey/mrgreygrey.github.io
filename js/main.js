be8825c953bd5abf586fb0497ce70084004b4dd2
mrgreygrey <gfairclough2@my.bcit.ca>
2019-03-27 07:22:29 -0700

    final version before submitting
    Added firebase support for one of the forms

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+//tests to see if the user understands that booleans deal with true and false
 function testBoolean(){
     var isBoolean = document.getElementById("booleanAnswer").value;
     var answer =isBoolean.toLowerCase();
@@ -10,6 +11,7 @@ function testBoolean(){
     }
 }
 
+//tests to see if the user understands the double is used for floating points
 function testDouble(){
     var isDouble = document.getElementById("doubleAnswer").value;
     var answer = isDouble.toLowerCase();
@@ -23,6 +25,7 @@ function testDouble(){
     }
 }
 
+//Tests to see if the user understands that the data is contained in the model
 function testModel(){
     var isModel = document.getElementById("modelAnswer").value;
     var answer = isModel.toLowerCase();
@@ -35,6 +38,7 @@ function testModel(){
     }
 }
 
+//tests to see if the user understands which part of the MVVM the user interacts with
 function testView(){
     var isView = document.getElementById("viewAnswer").value;
     var answer = isView.toLowerCase();
@@ -47,6 +51,7 @@ function testView(){
     }
 }
 
+//test to see if the user understands the concatenation symbol
 function testPlus(){
     var isPlus = document.getElementById("plusAnswer").value;
     if(isPlus == "+"){
@@ -58,10 +63,59 @@ function testPlus(){
     }
 }
 
+//tests to see if user understands the equal to symbol
+function testEqualTo(){
+    var isEqualTo = document.getElementById("equalToAnswer").value;
+    if(isEqualTo == "=="){
+        document.getElementById("equalToResult").innerHTML = "Correct";
+        document.getElementById("equalToResult").style.color = "green";
+    } else {
+        document.getElementById("equalToResult").innerHTML = "Not Quite! Try again!";
+        document.getElementById("equalToResult").style.color = "red";
+    }
+}
+
+//used to open navigation menu when in mobile browser
 function openNav(){
     document.getElementById("myDynamicNav").style.width = "40vw";
 }
 
+//used to close navigation menu when in mobile browser
 function closeNav(){
     document.getElementById("myDynamicNav").style.width = "0";
+}
+
+function addData(){
+    var db = firebase.firestore();
+
+    var first_Name = document.getElementById("firstName").value;
+    var last_Name = document.getElementById("lastName").value;
+    var _address = document.getElementById("address").value;
+    var _DOB = document.getElementById("dateOfBirth").value;
+    var _email = document.getElementById("email").value;
+    var _recieveNews = document.getElementById("recieveNews").checked;
+
+
+    alert(_recieveNews);
+
+    db.collection("joinFormData").add({
+        firstName: first_Name,
+        lastName: last_Name,
+        address: _address,
+        DOB: _DOB,
+        email: _email,
+        recieveNews: _recieveNews
+    })
+    .then(function(docRef){
+        console.log("written with Id:", docRef.id);
+    })
+
+    if(_recieveNews){
+        document.getElementById("thanksJoining").innerHTML = "Thanks " + first_Name + 
+        " for joining our group! You'll get your first newsletter shortly!";
+    } else {
+        document.getElementById("thanksJoining").innerHTML = "Thanks " + first_Name +
+        " for joining our group!";
+    }
+
 }
